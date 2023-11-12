09f3cc38e96d14c4963a2a2668e93fe8d46035c7
Pablo Uribe <pablo.um00@gmail.com>
2023-11-11 22:37:55 -0500

    Create project

---
+++ MazeResolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("This is the Maze Resolver!");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
