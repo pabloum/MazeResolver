91adca82e785123b6f36531b51ded9f46b455c85
Pablo Uribe <pablo.um00@gmail.com>
2023-11-12 13:41:04 -0500

    Add Dependency Injection

--- MazeResolver/Program.cs
+++ MazeResolver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Providers;
 
 namespace MazeResolver;
 
@@ -6,8 +7,27 @@ class Program
 {
     static async Task Main()
     {
-        Console.WriteLine("This is the Maze Resolver!");
+        Console.WriteLine("This is the Maze Resolver Challenge!");
+
+        var serviceProvider = SetUpDependencyInjection();
+
+        var mazeProvider = serviceProvider.GetRequiredService<IMazeProvider>();
+        var gameProvider = serviceProvider.GetRequiredService<IGameProvider>();
+
+        var playGame = new PlayGame(mazeProvider, gameProvider);
+        await playGame.Play();
 
         Console.WriteLine("Press any key to exit.");
+    }
+
+    private static ServiceProvider SetUpDependencyInjection()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddTransient<IRequestHandler, RequestHandler>() 
+            .AddTransient<IMazeProvider, MazeProvider>() 
+            .AddTransient<IGameProvider, GameProvider>() 
+            .BuildServiceProvider();
+
+        return serviceProvider;
     }
 }
